ff15d97e8acc78e7cbbd67906f88c0a037f6635f
insomniac807 <{17027013}+{insomniac807}@users.noreply.github.com>
2022-07-22 01:44:21 +0100

    permissions granted in discord dev console. bot replies to channel messages. !!N.B. a bit buggy. sending multiple replies to one message!!

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ require('dotenv').config()
 
 const token = process.env.BOT_TOKEN;
 
-// const myIntents = new GatewayIntentBits();
-// myIntents.add();
-
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions]});
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent]});
 
 client.once('ready', () => {
     console.log("Client Ready...");
+});
+
+client.on('messageCreate', (message) => {
+    console.log(message.content);
+    message.channel.send(message.content);
 });
 
 client.login(token);
