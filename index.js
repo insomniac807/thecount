6a1dd327fbde6ad22e30610d2a184728e650b441
insomniac807 <{17027013}+{insomniac807}@users.noreply.github.com>
2022-07-22 21:17:48 +0100

    checks if message is valid mathemtaical expression

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { evaluate } = require('mathjs');
 require('dotenv').config()
 
 const CHANNEL = process.env.CHANNEL;
 const token = process.env.BOT_TOKEN;
+
+const isMathExpression = (expression) => {
+    try {
+        evaluate(expression);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
 
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.MessageContent]});
 
@@ -13,7 +23,7 @@ client.once('ready', () => {
 client.on('messageCreate', (message) => {
     if((message.author.bot) || (message.channel.name.localeCompare(CHANNEL) != 0)) return;
     console.log(message.content);
-    message.channel.send(`channel: ${message.channel.name}`);
+    message.channel.send(`channel: ${isMathExpression(message.content)}`);
 });
 
 client.login(token);
